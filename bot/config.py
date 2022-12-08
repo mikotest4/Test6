9fb750ededd180e5e30641682933eae50b625c99
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2022-12-08 20:48:53 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,7 +6,7 @@ from decouple import config
 try:
     APP_ID = config("APP_ID", "6534707",cast=int)
     API_HASH = config("API_HASH","4bcc61d959a9f403b2f20149cbbe627a")
-    BOT_TOKEN = config("BOT_TOKEN","5271146077:AAFw5ZZK731UISWBTCBPDZPeDEVumtO-iFU")
+    BOT_TOKEN = config("BOT_TOKEN","5262012351:AAGTU69K3bKjlCf2-7Zek6hNkmp83PbpXSA")
     DEV = 1664850827
     OWNER = config("OWNER","1430593323")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]

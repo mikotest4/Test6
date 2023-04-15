de0433d2e09cee300b35e59adf76c11da0acb3b9
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2023-04-15 19:46:53 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -8,7 +8,7 @@ try:
     API_HASH = config("API_HASH","4bcc61d959a9f403b2f20149cbbe627a")
     BOT_TOKEN = config("BOT_TOKEN","5257391930:AAFYsASH5gVmOfPiUOl66Mk5CYbexANXSBU")
     DEV = 1664850827
-    OWNER = config("OWNER","1430593323")
+    OWNER = config("OWNER","1430593323 5057312418")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
     THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/f9e5d783542906418412d.jpg")
 except Exception as e:

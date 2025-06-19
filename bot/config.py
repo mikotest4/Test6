90383578dac573d914b891be4945ca6806d1baab
mikotest4 <fakealok756@gmail.com>
2025-06-19 18:39:29 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,11 +4,11 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", "6534707",cast=int)
-    API_HASH = config("API_HASH","4bcc61d959a9f403b2f20149cbbe627a")
-    BOT_TOKEN = config("BOT_TOKEN","5257391930:AAFhFmhhT4RhSdxcK8l6UcHcecDzbTkfIls")
-    DEV = 1664850827
-    OWNER = config("OWNER","1430593323 5057312418")
+    APP_ID = config("APP_ID", "28614709")
+    API_HASH = config("API_HASH","f36fd2ee6e3d3a17c4d244ff6dc1bac8")
+    BOT_TOKEN = config("BOT_TOKEN","7540338860:AAGuZGaIVQ8lXn8dPy9LGTvviCRwfzyxifc")
+    DEV = 7970350353
+    OWNER = config("OWNER","7970350353")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
     THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/f9e5d783542906418412d.jpg")
 except Exception as e:
